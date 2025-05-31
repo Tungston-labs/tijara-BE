5be371c95b7399354bba99ab464d8bae39bda61d
Ajay-tungston <you@example.com>
2025-05-31 16:52:36 +0530

    File uploading is in progress

--- controllers/admin/authController.js
+++ controllers/admin/authController.js
@@ -320,8 +320,8 @@ const addSellerByAdmin = async (req, res, next) => {
     }
 
     const baseUrl = `${req.protocol}://${req.get("host")}`;
-    const tradeLicensePath = `${baseUrl}/uploads/compressed/licenses/${req.files.tradeLicenseCopy[0].filename}`;
-    const profileImagePath = `${baseUrl}/uploads/compressed/users/sellers/${req.files.profileImage[0].filename}`;
+    const tradeLicensePath = `${baseUrl}/uploads/licenses/${req.files.tradeLicenseCopy[0].filename}`;
+    const profileImagePath = `${baseUrl}/uploads/users/sellers/${req.files.profileImage[0].filename}`;
 
     // Input validations
     if (!usernameRegex.test(name)) {
