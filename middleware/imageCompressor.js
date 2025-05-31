73a6942f28ad36bcb88909c31ddbc0baddfde66a
Ajay-tungston <you@example.com>
2025-05-31 16:44:31 +0530

    File is missing

--- middleware/imageCompressor.js
+++ middleware/imageCompressor.js
@@ -18,7 +18,7 @@ const compressUploadedImages = async (req, res, next) => {
 
       const inputPath = file.path;
       const ext = path.extname(inputPath);
-      const compressedDir = path.dirname(inputPath).replace("uploads", "uploads/compressed");
+      const compressedDir = path.dirname(inputPath).replace("uploads", "uploads/");
       const outputFilename = `${Date.now()}-${file.filename}`;
       const outputPath = path.join(compressedDir, outputFilename);
 
