4a6c2d81e698cea72b2bd245d5c820df2beeeb7f
Ajay-tungston <you@example.com>
2025-06-04 10:55:01 +0530

    Change a validation

--- controllers/Login/loginController.js
+++ controllers/Login/loginController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../../models/User");
 const { sendOTP,verifyOTP } = require("../services/twilio");
 
 const { isValidNumber, parsePhoneNumber } = require('libphonenumber-js');
