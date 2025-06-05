a244b7777c85779118ecad7ab65a0adfe614431c
Ajay-tungston <you@example.com>
2025-06-05 11:00:14 +0530

    location set up is changed

--- routes/location/locationRoutes.js
+++ routes/location/locationRoutes.js
@@ -1,8 +1,14 @@
 const express= require("express");
 const { createLocation, getAllLocations } = require("../../controllers/location/locationController");
 const router=express.Router()
-
-router.post('/create-location',createLocation);
-router.get('/get-location',getAllLocations);
+const jwtAuthentication = require("../../middleware/jwtAuthentication");
+const verifyAdmin = (req, res, next) => {
+    if (req.user.role !== "admin") {
+      return res.status(403).json({ message: "Access denied" });
+    }
+    next();
+  };
+router.post('/create-location',createLocation,jwtAuthentication,verifyAdmin);
+router.get('/get-location',getAllLocations, jwtAuthentication, verifyAdmin);
 
 module.exports = router;
