6f25582f9a1f380bdba648697b0faf7bb5494d47
Ajay-tungston <you@example.com>
2025-06-06 11:19:52 +0530

    axios error fixed

--- controllers/location/locationController.js
+++ controllers/location/locationController.js
@@ -1,10 +1,10 @@
-const { Axios } = require("axios");
+const axios = require("axios");
 
 const reverseGeocode = async (latitude, longitude) => {
   const apiKey = process.env.OPENCAGE_API_KEY;
   const url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKey}`;
 
-  const response = await Axios.get(url);
+  const response = await axios.get(url);
   const address = response.data?.results?.[0]?.formatted || "Unknown Location";
   return address;
 };
