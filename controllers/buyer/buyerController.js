2d4720681dd027037bfd0fea3a423be1f1e8041f
Ajay-tungston <you@example.com>
2025-06-06 11:23:49 +0530

    axios error fixed

--- controllers/buyer/buyerController.js
+++ controllers/buyer/buyerController.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const validator = require("validator");
 const Location = require("../../models/Location");
-const axios = require("axios");
 const reverseGeocode = require("../location/locationController")
 
 const usernameRegex = /^[a-zA-Z0-9_ ]{3,50}$/;
