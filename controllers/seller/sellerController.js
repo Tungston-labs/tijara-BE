ba101eb30c532810a8f51cbd3bc9503f92176e33
Ajay-tungston <you@example.com>
2025-06-25 13:12:23 +0530

    It is not working coprrectly

--- controllers/seller/sellerController.js
+++ controllers/seller/sellerController.js
@@ -7,25 +7,10 @@ const Location = require("../../models/Location");
 const axios = require("axios");
 
 const usernameRegex = /^[a-zA-Z0-9 ]+$/;
+const phoneRegex = /^[0-9]{10}$/;
+
 const passwordRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/;
-
-const phoneValidators = {
-  IN: /^(\+91)?[6-9][0-9]{9}$/,
-  UAE: /^(\+971)?(50|52|54|55|56)[0-9]{7}$/,
-  SA: /^(\+966)?5[0-9]{8}$/,
-  QA: /^(\+974)?(3|5|6|7)[0-9]{7}$/,
-  OM: /^(\+968)?(7[1-9]|9[1-9])[0-9]{6}$/,
-  KW: /^(\+965)?(5|6|9)[0-9]{7}$/,
-  BH: /^(\+973)?(3|6|7)[0-9]{7}$/,
-};
-const isValidPhone = (phone, country) => {
-  const regex = phoneValidators[country?.toUpperCase()];
-  if (!regex) {
-    return false; // Unknown country code
-  }
-  return regex.test(phone);
-};
 
 const reverseGeocode = async (latitude, longitude) => {
   const apiKey = process.env.OPENCAGE_API_KEY;
@@ -105,10 +90,10 @@ const registerSeller = async (req, res, next) => {
       return res.status(400).json({ message: "Invalid name format" });
     if (!validator.isEmail(email))
       return res.status(400).json({ message: "Invalid email" });
-    if (!isValidPhone(phone, country)) {
-      return res.status(400).json({
-        message: "Invalid phone number format for selected country.",
-      });
+    if (!phoneRegex.test(phone)) {
+      return res
+        .status(400)
+        .json({ message: "Phone must be 10 digits, numbers only." });
     }
     if (!passwordRegex.test(password)) {
       return res.status(400).json({
